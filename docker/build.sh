2a0b9053ea6690433e1d78f40675533f6fedc1dc
Travis CI <nobody@nobody.org>
2017-05-29 22:18:46 -0300

    Construção funcional

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd "$(dirname ${0})"
 docker build -q --force-rm=true --rm=true -t erikliberal/jdk8 jdk8
-docker build -q --force-rm=true --rm=true -t erikliberal/gocd-server gocd-server
+docker build -q --force-rm=true --rm=true -t erikliberal/jre8 jre8
+docker build --force-rm=true --rm=true -t erikliberal/gocd-server gocd-server
+docker build --force-rm=true --rm=true -t erikliberal/gocd-server-instance gocd-server-instance
