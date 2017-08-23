cb95b330239b1e839c97f33021cfb1effe3be8db
Erik Liberal <erik.liberal@gmail.com>
2017-08-22 22:29:01 -0300

    Adição de serviço do gitlab

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,12 @@ if [ "x$(ps --no-headers -o comm 1)x"=="xsystemdx"  ] && [ "$(systemctl is-activ
     sudo systemctl start docker.service;
 fi
 cd "$(dirname ${0})"
+if [ "x$(docker volume ls | grep -o 'gitlab-data')x" == 'xx' ] ; then
+  docker volume create --name gitlab-data
+fi
+
+echo "gitlab [ $(docker run -d --rm --name gitlab -e GITLAB_ROOT_PASSWORD='erikliberal@infoxx2017!' -e GITLAB_ROOT_EMAIL='erik.liberal@gmail.com' -v gitlab-data:/var/opt/gitlab gitlab/gitlab-ce:9.4.5-ce.0) ] on ip < $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gitlab) >"
+
 if [ "x$(docker volume ls | grep -o 'nexus-data')x" == 'xx' ] ; then
   docker volume create --name nexus-data
 fi
