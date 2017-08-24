29b51e9b59abf910873661e3ca6b8f38fb9bce68
Erik Liberal <erik.liberal@gmail.com>
2017-08-23 21:56:24 -0300

    Atualização em scripts de inicialização para criar servidores em rede isolada com aliases definidos dentro dela

--- docker/run.sh
+++ docker/run.sh
@@ -4,21 +4,30 @@ if [ "x$(ps --no-headers -o comm 1)x"=="xsystemdx"  ] && [ "$(systemctl is-activ
     sudo systemctl start docker.service;
 fi
 cd "$(dirname ${0})"
-if [ "x$(docker volume ls | grep -o 'gitlab-data')x" == 'xx' ] ; then
+
+user_created_network_name=redeInfraInterna
+
+if [ 0 -eq $(docker network ls | grep -c $user_created_network_name) ] ; then
+    echo "Criando rede $user_created_network_name";
+    docker network create --driver bridge $user_created_network_name
+fi
+
+if [ 0 -eq $(docker volume ls | grep -c 'gitlab-data') ] ; then
   echo 'Criando volume para persistir os dados do gitlab entre inicializações'
   docker volume create --name gitlab-data
 fi
-
-echo "gitlab [ $(docker run -d --rm --name gitlab -e GITLAB_ROOT_PASSWORD='erikliberal@infoxx2017!' -e GITLAB_ROOT_EMAIL='erik.liberal@gmail.com' -v gitlab-data:/var/opt/gitlab gitlab/gitlab-ce:9.4.5-ce.0) ] on ip < $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gitlab) >"
+echo "gitlab [ $(docker run -d --rm --name gitlab --network $user_created_network_name --network-alias gitlab -e GITLAB_ROOT_PASSWORD='erikliberal@infoxx2017!' -e GITLAB_ROOT_EMAIL='erik.liberal@gmail.com' -v gitlab-data:/var/opt/gitlab gitlab/gitlab-ce:9.4.5-ce.0) ] on ip < $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gitlab) >"
 
 if [ "x$(docker volume ls | grep -o 'nexus-data')x" == 'xx' ] ; then
   echo 'Criando volume para persistir os dados do nexus entre inicializações'
   docker volume create --name nexus-data
 fi
-echo "nexus [ $(docker run -d --rm --name nexus -v nexus-data:/nexus-data sonatype/nexus3) ] on ip < $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nexus) >"
+echo "nexus [ $(docker run -d --rm --name nexus --network $user_created_network_name --network-alias nexus -v nexus-data:/nexus-data sonatype/nexus3) ] on ip < $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nexus) >"
+
 if [ "x$(docker volume ls | grep -o 'jenkins-data')x" == 'xx' ] ; then
     echo 'Criando volume para persistir os dados do jenkins entre inicializações'
     docker volume create --name jenkins-data
 fi
-echo "jenkins [ $( docker run -d --rm --name=jenkins --link gitlab:gitlab --link nexus:nexus -v jenkins-data:/var/jenkins_home jenkins/jenkins:2.60.2 ) ] on ip < $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jenkins) >"
-#docker cp .m2 jenkins:/root/.m2
+echo "jenkins [ $( docker run -d --rm --name=jenkins --network $user_created_network_name --network-alias jenkins -v jenkins-data:/var/jenkins_home jenkins/jenkins:2.60.2 ) ] on ip < $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jenkins) >"
+docker exec jenkins mkdir -p /var/jenkins_home/.m2
+docker cp settings.xml jenkins:/var/jenkins_home/.m2/settings.xml
